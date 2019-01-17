3f671726b7a69c528562b4c5e0e2f93009f3644c
German27 <german.velasco98@gmail.com>
2019-01-17 16:26:01 -0300

    Products loaded

--- js/products.js
+++ js/products.js
@@ -1,9 +1,32 @@
 $('#btn-nav').click(function () {
-  if ($('#btn-nav').hasClass("active")) {
-    $('#sidebar').hide()
-    $('#btn-nav').removeClass("active")
-  } else {
     $('#sidebar').show()
     $('#btn-nav').addClass("active")
-  }
+    $('#btn-nav').hide()
+    $('#btn-close').show()
 });
+
+$('#btn-close').click(function () {
+  $('#sidebar').hide()
+  $('#btn-nav').show()
+  $('#btn-close').hide()
+});
+
+$.getJSON("products.json", function (json) {
+
+  for (let i = 0; i < json.ProductCollection.length; i++) {
+    const product = json.ProductCollection[i];
+
+    var html = '<div class="jr-flex"> <p class="product-name">' + product.Name + '</p>' +
+      '<img src="images/default.jpg" alt="product">' +
+      '<p class="product-description">' + product.Description + '</p>' +
+      '<p class="product-price">$' + product.Price + '</p>' +
+      '<button class="btn-product"> Agregar al carrito</button>' +
+      '<a href="#" class="product-info">Ver mas</a>' +
+      '</div>'
+
+    $('.flex-container').append(html);
+
+  }
+
+});
+

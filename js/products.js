794712c84486ddf2d658e8750b5eb8aa2d956ccc
German27 <german.velasco98@gmail.com>
2019-01-18 11:06:16 -0300

    Modal box

--- js/products.js
+++ js/products.js
@@ -16,13 +16,25 @@ $.getJSON("products.json", function (json) {
   for (let i = 0; i < json.ProductCollection.length; i++) {
     const product = json.ProductCollection[i];
 
-    var html = '<div class="jr-flex"> <p class="product-name">' + product.Name + '</p>' +
-      '<img src="images/default.jpg" alt="product">' +
-      '<p class="product-description">' + product.Description + '</p>' +
-      '<p class="product-price">$' + product.Price + '</p>' +
-      '<button class="btn-product"> Agregar al carrito</button>' +
-      '<a href="#" class="product-info">Ver mas</a>' +
-      '</div>'
+    let html =  '<div class="jr-flex"> <p class="product-name">' + product.Name + '</p>' +
+                  '<img src="images/default.jpg" alt="product">' +
+                  '<p class="product-description">' + product.Description + '</p>' +
+                  '<p class="product-price">$' + product.Price + '</p>' +
+                  '<button class="btn-product"> Agregar al carrito </button>' +
+                  '<a href="product/' + product.ProductId + '" class="product-info">Ver mas</a>' +
+                  '<div id="product-modal" class="modal">' +
+                    '<div class="modal-content">' + 
+                      '<div class="modal-body">' + 
+                      '<img src="images/marker.png" alt="check-mark">' +
+                      '<p> Tu producto fue agregado al carrito con exito </p>' +
+                      '</div>' +
+                      '<div class="modal-footer">' +
+                        '<button class=""> Ir al carrito </button>' +
+                        '<button class="close-modal"> Seguir comprando </button>' +
+                      '</div>' +
+                    '</div>'
+                  '</div>' +
+                '</div>'
 
     $('.flex-container').append(html);
 
@@ -30,3 +42,10 @@ $.getJSON("products.json", function (json) {
 
 });
 
+$(document).on('click', '.btn-product', function () {
+  $('#product-modal').show()
+});
+
+$(document).on('click', '.close-modal', function () {
+  $('#product-modal').hide()
+});
